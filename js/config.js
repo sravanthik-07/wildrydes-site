caaca1c04cadc38b823d06a474bbaa7c01282ed9
Sravanthi Koneru <sravanthi.koneru07@gmail.com>
2026-01-07 13:24:25 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ap-south-1_rSsFOOnYB', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3cjconc1p416qbtis1s7jcdbc8', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
