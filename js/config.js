39564c4ecba33b8c10818e088d098bd56b2bb1ce
Sravanthi Koneru <sravanthi.koneru07@gmail.com>
2026-01-21 12:16:57 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,13 +1,14 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_hDsIkPSr5', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '27b1abrq389qqchsued7ec4ovh', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'ap-south-1_nKDqMfNt1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '22en5a5cm8pe7c6k8is3i5ibt9', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'ap-south-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
 
 
